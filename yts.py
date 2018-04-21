a63ac7224bdf379a0a199db13e6de2768e2556fe
kumbasar <vkumbasar@outlook.com>
2018-04-21 10:26:47 +0300

    Working release

--- yts.py
+++ yts.py
@@ -4,12 +4,17 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-YTS_AG = "https://yts.am/api/v2/list_movies.json?minimum_rating=7&sort_by=date_added&genre=Documentary"
+YTS_AG_PARMS = "?minimum_rating=7&sort_by=date_added&genre=Documentary"
+YTS_AG_API = "https://yts.am/api/v2/list_movies.json"
 
-req = Request(YTS_AG, headers={'User-Agent': 'Mozilla/5.0'})
+req = Request(YTS_AG_API+YTS_AG_PARMS, headers={'User-Agent': 'Mozilla/5.0'})
 
 with urlopen(req) as url:
     data = json.loads(url.read().decode())
 
-print(data)
+print('Title'.ljust(60) + 'Year'.ljust(6) + 'Rating'.ljust(10) + 'URL')
+
+for i in data['data']['movies']:
+    print(str(i['title']).ljust(60) + str(i['year']).ljust(6) + str(i['rating']).ljust(10) + str(i['url']).ljust(4))
+
     
